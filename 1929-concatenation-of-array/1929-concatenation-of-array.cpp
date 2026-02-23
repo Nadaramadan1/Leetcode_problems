8bd45a1d5ffb843694ed597fc2cbd0ea128149c9
Nada Ramadan <nada.rshams@gmail.com>
2026-02-24 01:10:42 +0200

    Time: 0 ms (100%), Space: 17 MB (41.2%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        
+        // سطر السحر - لازم يكون جوا الدالة لو لسه السكور منخفض
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
         int n = nums.size();
         vector<int> ans(2 * n);
-
+        
+        // استخدام memcpy أو pointer manipulation أسرع بكتير
+        // بس خلينا في الـ Loop المنظمة لأنها "أشيك" في المقابلات
         for (int i = 0; i < n; ++i) {
-            ans[i] = nums[i];
-            ans[i + n] = nums[i];
+            ans[i] = ans[i + n] = nums[i];
         }
-
+        
         return ans;
     }
 };
