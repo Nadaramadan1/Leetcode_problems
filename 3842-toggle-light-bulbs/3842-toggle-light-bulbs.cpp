e28be6e1e637db5865da0e93d96aa4b6625e9d6d
Nada Ramadan <nada.rshams@gmail.com>
2026-02-25 22:47:46 +0200

    Time: 0 ms (100%), Space: 33.7 MB (63.25%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// تسريع الـ I/O لضمان 100% Beats
+// السر الحقيقي لـ 0ms في LeetCode
 static const int speedup = []() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -12,16 +12,17 @@ static const int speedup = []() {
 class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
-     
+        // مصفوفة ثابتة سريعة جداً
         bool isOn[101] = {false};
         
-       
         for (int b : bulbs) {
             isOn[b] = !isOn[b]; 
         }
         
-       
         vector<int> result;
+        // حركة ذكية: نحجز مساحة مسبقاً لتقليل الـ Overhead
+        result.reserve(100); 
+        
         for (int i = 1; i <= 100; ++i) {
             if (isOn[i]) {
                 result.push_back(i);
